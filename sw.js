ba745da4ee926891ff6bffb802298a21be46f2c0
3schools <85482920+3schools@users.noreply.github.com>
2021-06-07 19:18:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      'index.html',
+      'fox-icon.png',
+      'index.js',
+      'style.css',
+      'fox1.jpg',
+      'fox2.jpg',
+      'fox3.jpg',
+      'fox4.jpg',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});

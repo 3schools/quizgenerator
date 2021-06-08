1e46b004e2f3f158e4b0ba894f1ab84c71b1ca34
3schools <85482920+3schools@users.noreply.github.com>
2021-06-08 11:32:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,9 +6,7 @@ self.addEventListener('install', (e) => {
       'https://raw.githubusercontent.com/3schools/quizgenerator/main/index.js',
       'https://raw.githubusercontent.com/3schools/quizgenerator/main/style.css',
       'http://4.bp.blogspot.com/-iv5mn-sqnlA/YL8F5Ehq-6I/AAAAAAAABos/m3Yj-LLaVnceGBgVNy-E2stfLmkr8YPIgCK4BGAYYCw/s1600/ask.png',
-      'fox2.jpg',
-      'fox3.jpg',
-      'fox4.jpg',
+      
     ])),
   );
 });

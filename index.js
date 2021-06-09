285feb80db29f65ee3129553873034688926b8ca
3schools <85482920+3schools@users.noreply.github.com>
2021-06-09 12:41:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,16 +34,16 @@ window.addEventListener('beforeinstallprompt', (e) => {
   addBtn.style.display = 'block';
 
   addBtn.addEventListener('click', () => {
-    // hide our user interface that shows our A2HS button
+    // hide our user interface that shows our  button
     addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
+        console.log('User Download ask 3schools');
       } else {
-        console.log('User dismissed the A2HS prompt');
+        console.log('User will download ask 3schools');
       }
       deferredPrompt = null;
     });
